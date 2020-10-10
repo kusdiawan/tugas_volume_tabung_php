00c42f2b6b2fb72ca231d8baeaad3b38049f4293
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-10 17:03:44 +0700

    Update tugas_volume_tabung.php

--- tugas_volume_tabung.php
+++ tugas_volume_tabung.php
@@ -3,11 +3,15 @@ $jari2 = 10;
 $tinggi = 30;
 const phi = 22/7;
 
-function pow_luas_alas($jari2, $pangkat){
-	return pow($jari2, $pangkat);
+function luas_lingkaran($jari2){
+  return phi * $jari2 * $jari2;
+}
+
+function volume_tabung($tinggi, $jari2){
+  return round($tinggi*luas_lingkaran($jari2),2);
 }
 
 echo "<br> <h4>Tugas menghitung Volume Tabung</h4>";
-echo "<br> Jadi Volume dari tabung tersebut adalah : ", number_format((phi *(pow_luas_alas($jari2, 2)) * $tinggi), 2);  
+echo "<br> Jadi Volume tabung tersebut adalah : ", number_format(volume_tabung($tinggi, $jari2), 2);  
 
 ?>
